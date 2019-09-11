9aa3b4ef38ff87c74714c0c67f43c11faebb63e7
admin <admin@admindeMacBook-Pro.local>
2019-09-11 23:14:04 +0800

    修改了一些东西

--- demo5/lib/keep_page.dart
+++ demo5/lib/keep_page.dart
@@ -5,8 +5,10 @@ class KeepPage extends StatefulWidget {
   _KeepPageState createState() => _KeepPageState();
 }
 // 混入继承保持页面状态的类
+//混入AutomaticKeepAliveClientMixin，这是保持状态的关键
 class _KeepPageState extends State<KeepPage> with AutomaticKeepAliveClientMixin {
   int addNume = 0;
+  //然后重写wantKeppAlive 的值为true
   @override
    bool get wantKeepAlive => true;
 
@@ -17,8 +19,10 @@ class _KeepPageState extends State<KeepPage> with AutomaticKeepAliveClientMixin 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //增加一个悬浮按钮，点击时触发_incrementCounter方法
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        tooltip: 'Increment',
         onPressed: (){
           setState(() {
             addNume++;
@@ -28,12 +32,14 @@ class _KeepPageState extends State<KeepPage> with AutomaticKeepAliveClientMixin 
       body: Column( // 竖
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Center(
-            child: Text('数量',style: TextStyle(fontSize: 30.0)),
-          ),
-           Center(
-            child: Text('$addNume',style: TextStyle(fontSize: 30.0))
-            )
+           Text('$addNume',style: Theme.of(context).textTheme.display3),
+           Text('点一下加一')
+          // Center(
+          //   child: Text('数量',style: TextStyle(fontSize: 30.0)),
+          // ),
+          //  Center(
+          //   child: Text('$addNume',style: TextStyle(fontSize: 30.0))
+          //   )
         ],
       ),
     ); 
